696d859fdb880d4d853c97f24ec441f10f9583b6
unknown <ayobami.oladosu@gmail.com>
2020-06-23 15:31:14 +0100

    j

---
+++ build/precache-manifest.7fe3894f84b77a6278f6540f4294e726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2caaec2f2ecce973c42c29ba37972634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7788f3cd038c1ebc8283",
+    "url": "/static/css/main.793775df.chunk.css"
+  },
+  {
+    "revision": "1e9cb36cc3e56c5e93f5",
+    "url": "/static/js/2.2779ed47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2779ed47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7788f3cd038c1ebc8283",
+    "url": "/static/js/main.0c087e9f.chunk.js"
+  },
+  {
+    "revision": "07e9b86387fa5613a913",
+    "url": "/static/js/runtime-main.4910fa9c.js"
+  },
+  {
+    "revision": "9e693df1067f9d47fe43534586c2becb",
+    "url": "/static/media/logo.9e693df1.png"
+  }
+]);
